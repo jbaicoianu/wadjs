4b9752e8072f3dfe6a3adc4e14a92b1a9402acdb
James Baicoianu <james_github@baicoianu.com>
2017-09-23 17:21:26 -0700

    Added more class documentation

--- src/objects/boundingbox.js
+++ src/objects/boundingbox.js
@@ -1,5 +1,8 @@
 import * as WadJS from '../wad.js';
 
+/**
+ * Class representing a BoundingBox
+ */
 export class BoundingBox {
   constructor() {
     this.x1 = 0;
@@ -7,15 +10,33 @@ export class BoundingBox {
     this.x2 = 0;
     this.y2 = 0;
   }
+
+  /**
+   * Read binary data from the given position
+   * @param {ArrayBuffer} data - binary data
+   * @param {integer} pos - position to read from
+   */
   read(data, pos) {
     this.x1 = WadJS.readInt16(data, pos);
     this.y1 = WadJS.readInt16(data, pos + 2);
     this.x2 = WadJS.readInt16(data, pos + 4);
     this.y2 = WadJS.readInt16(data, pos + 6);
   }
+
+  /**
+   * Get the byte size of this object
+   * @returns {integer} number of bytes
+   */
   getByteSize() {
     return 8;
   }
+
+  /**
+   * Determine whether the specified point falls within this bounding box
+   * @returns {integer} x - x position
+   * @returns {integer} y - y position
+   * @returns {boolean} point is contained within
+   */
   containsPoint(x, y) {
     return (x >= this.x1 && x <= this.x2 && y >= this.y1 && y <= this.y2);
   }
