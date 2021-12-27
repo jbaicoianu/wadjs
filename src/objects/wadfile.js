23466621f6433bcc12f672be7b3f1c839a0e18ff
James Baicoianu <james_github@baicoianu.com>
2021-12-27 14:43:03 -0800

    Moved utility functions into mathutils module

--- src/objects/wadfile.js
+++ src/objects/wadfile.js
@@ -333,21 +333,5 @@ export class WadFile {
       pcm: pcmdata
     };
   }
-
-  /**
-   * Utility function to determine how far a point is from a line
-   * @todo Move this into a separate module
-   */
-  pointDistanceFromLine(px, py, lx, ly, dx, dy) {
-    var d = (px - lx) * dy - (py - ly) * dx;
-    return d;
-  }
-  /**
-   * Utility function to determine if a point is on a line
-   * @todo Move this into a separate module, and fill in the logic
-   */
-  pointIsOnLine(px, py, lx, ly, dx, dy) {
-    return false;
-  }
 }
 
